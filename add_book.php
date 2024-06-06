1045955414e66255f92f87c49f7f58fba00b7ced
Berkay Muhcu <71499075+berkaymhc@users.noreply.github.com>
2024-06-06 10:28:51 +0300

    Update add_book.php
    sql injection added

--- add_book.php
+++ add_book.php
@@ -2,10 +2,10 @@
 include 'db_connect.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $title = $_POST['title'];
-    $author = $_POST['author'];
-    $description = $_POST['description'];
-    $otomat = $_POST['otomat'];
+    $title = $conn->real_escape_string($_POST['title']);
+    $author = $conn->real_escape_string($_POST['author']);
+    $description = $conn->real_escape_string($_POST['description']);
+    $otomat = $conn->real_escape_string($_POST['otomat']);
 
     $sql = "INSERT INTO books (title, author, description, otomat) VALUES ('$title', '$author', '$description', '$otomat')";
 
